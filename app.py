2ea13bdab38eaf7fc100ae389f5c0b967c80869f
VRAI-02 <raivikash0207@gmail.com>
2025-04-13 16:08:40 +0000

    change in database

--- app.py
+++ app.py
@@ -1,32 +1,18 @@
 from flask import Flask , render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Data Analyst',
-    'location': 'Bengaluru, India',
-  },
-  {
-    'id': 2,
-    'title': 'Data Scientist',
-    'location': 'Delhi, India',
-  },
-  {
-    'id':3,
-    'title': 'Frontend Engineer',
-    'location': 'Remote',
-  }
-]
 
 @app.route("/")
 def hello_job():
-  return render_template('home.html', jobs=JOBS, company_name='Career')
+  jobs = load_jobs_from_db()
+  return render_template('home.html', jobs=jobs, company_name='Career')
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify (JOBS)
+  jobs= load_jobs_from_db()
+  return jsonify (jobs)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
