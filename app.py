5178d0eb6adc9506119a192c14b067a171d62867
VRAI-02 <raivikash0207@gmail.com>
2025-04-14 16:18:12 +0000

    Store application in database

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask , render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask , render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
+
 
 app = Flask(__name__)
 
@@ -7,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_job():
   jobs = load_jobs_from_db()
-  return render_template('home.html', jobs=jobs, company_name='Career Marketplace')
+  return render_template('home.html', jobs=jobs, company_name='Career Marketplace')##company_name is a variable
 # routing to the api page for jobs and converting the data into json format
 @app.route("/api/jobs")
 def list_jobs():
@@ -20,6 +21,16 @@ def show_job (id):
   if not job:
     return "Not Found", 404
   return render_template('jobpage.html',job=job)
+
+# routing to the apply page posting data in a form
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_application_to_db(id,data)
+  #TODO - display an acknowledgement
+  #TODO - send an email
+  return render_template('application_submitted.html', application=data, job=job)
 ##host="0.0.0.0" makes the server accessible from any network interface, not just localhost.
 ##while launching in production,it is recommended to disable debug=flase mode since it can expose sensitive information.
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text,data
 import os
 # database connection secret key from the environment variable
 db_connection_string = os.environ['DB_CONNECTION_STRING']
@@ -9,7 +9,7 @@ engine = create_engine(db_connection_string, connect_args={
   }
 })
 
-# List of jobs --> fetching the value using mysql query from database, connecting engine and converting into a dictionary
+# Funcation to list of jobs --> fetching the value using mysql query from database, connecting engine and converting into a json dictionary
 def load_jobs_from_db():
  with engine.connect() as conn:
   result = conn.execute(text("select*from jobs"))
@@ -17,7 +17,17 @@ def load_jobs_from_db():
   for row in result.all():
     jobs.append(dict(row)) 
     return jobs
-# New page for listed job -->fetching the value using mysql query from database, connecting engine and converting into a dictionary
+# Funcation for new page for listed job --> fetching the value using mysql query from database, connecting engine and converting into a json dictionary
+# Create table jobs(
+# id Int not null Auto_increment,
+# title VARCHAR (50) Not null,
+# Location VARCHAR (50) Not Null,
+# Salary INT,
+# Currency VARCHAR(10),
+# responsibility VARCHAR(200),
+# requirement VARCHAR (200),
+# PRIMARY KEY (id)
+# );
 def load_job_from_db(id):
   with engine.connect() as conn:
     result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),
@@ -27,3 +37,38 @@ def load_job_from_db(id):
       return None
     else:
       return dict(rows[0])
+#Funcation to add submitted aplication form data to the database
+# Create table applicaton(
+#   id Int not null Auto_increment,
+#   job_id INT NOT NULL,
+#   Full_name VARCHAR (50) Not null,
+#   email VARCHAR (50) Not Null,
+#   linkedin_url VARCHAR (50),
+#   work_experience VARCHAR (5000),
+#   education VARCHAR (5000),
+#   resume_url VARCHAR (500),
+#   created_at TIMESTAMP DEFAULT current_timestamp,
+#   updated_at TIMESTAMP DEFAULT current_timestamp on update current_timestamp,
+#   PRIMARY KEY (id)
+# );
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO application (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+    conn.execute(query, {
+       "job_id": job_id,
+       "full_name": data['full_name'],
+       "email": data['email'],
+       "linkedin_url": data['linkedin_url'],
+       "work_experience": data['work_experience'],
+       "education": data['education'],
+       "resume_url": data['resume_url']
+       })
+#alternte way to add data to the database --> need to check which one is working correctly.
+    # conn.execute(query,
+    #              job_id=job_id,
+    #              full_name=data['full_name'],
+    #              email=data['email'],
+    #              linkedin_url=data['linkedin_url'],
+    #              work_experience=data['work_experience'],
+    #              education=data['education'],
+    #              resume_url=data['resume_url'])
